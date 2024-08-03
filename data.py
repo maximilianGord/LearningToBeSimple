e566547c45d732d7f5bd5f219e80e8cf07fb0f21
Max G <Max@mail.com>
2024-08-03 18:46:23 +0200

    DataLoader created / Data ready for pytorch according to Paper

--- data.py
+++ data.py
@@ -1,14 +1,17 @@
-from sage.all import *
+#from sage.all import *
 import numpy as np
 from itertools import chain,combinations
 import pandas as pd
+import ast
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 #python generat permutations
 def generateRandomPermutations(numberOf, order):
     """
         creates numberOf permutations with given order
     """
-    permutations = np.zeros((numberOf,order))
+    permutations = np.zeros((numberOf,order),dtype=int)
     for i in range(0,numberOf):
         indexlist = [i for i in range(order)]
         len_list = order
@@ -57,11 +60,28 @@ def createPermutationMatrix(permutation):
     for i in range(order):
         p_matrix[i,permutation[i]]=1
     return p_matrix
-#g1 = PermutationGroup([[3,1,2,0],[0,2,1,3]], domain=[0,1,2,3])
-#g1 = PermutationGroup([array([0., 2., 3., 1.]), array([0., 3., 2., 1.])], domain=[0,1,2,3])
-
+def flattenPermMatrix(pMatrix):
+    return pMatrix.flatten()
 def simpletest(cperms):
 	return list((map (lambda pi: PermutationGroup(pi,domain = list(range(len(pi[0])))).is_simple(), cperms)))
+def getDataLoader(dataframe):
+    X_raw = np.array(dataframe['X'].values)
+    X_raw = [elem[1:-1] for elem in X_raw]
+    X_raw = np.stack([np.array(ast.literal_eval(f"[{elem}]")) for elem in X_raw])
+    X_Matrix = np.apply_along_axis(createPermutationMatrix,axis=2,arr=X_raw)
+    #X_flat = np.apply_along_axis(flattenPermMatrix,axis=3,arr=X_Matrix)
+    orig_shape = X_Matrix.shape
+    new_shape = (orig_shape[0],orig_shape[1]*orig_shape[3]*orig_shape[2])
+    X_polished = X_Matrix.reshape(new_shape)
+    
+    y = np.array(dataframe['y'].values)
+
+    X = torch.tensor(X_polished,dtype=torch.int64)
+    y = torch.tensor(y,dtype=torch.int64)
+    print(X)
+    print(y)
+    loader = DataLoader(list(zip(X,y)),shuffle=True,batch_size=16)
+    return loader 
 
 
 
@@ -69,15 +89,16 @@ def simpletest(cperms):
 #               	    (lambda ro: convertPermutation(ro),
 #               	    generateRandomPermutations(6,4))
 #                 ))
-single_generators = generateRandomPermutations(6,4)
-combined_generators = list(combinations(single_generators,2))
-combined_generators = [[list(elem[0]),list(elem[1])] for elem in combined_generators]
+# single_generators = generateRandomPermutations(6,4)
+# combined_generators = list(combinations(single_generators,2))
+# combined_generators = [[list(elem[0]),list(elem[1])] for elem in combined_generators]
 
-combined_generators_df = pd.DataFrame({'X':combined_generators})
-print(len(combined_generators))
-print(combined_generators)
-mathpermssimple =  simpletest(combined_generators)
+# combined_generators_df = pd.DataFrame({'X':combined_generators})
+# mathpermssimple =  simpletest(combined_generators)
 
-combined_generators_df['y'] = mathpermssimple
-combined_generators_df.to_csv('data_1.csv')
+# combined_generators_df['y'] = mathpermssimple
+# combined_generators_df.to_csv('data_1.csv')
+combined_generators_df = pd.read_csv('data_1.csv')
+getDataLoader(combined_generators_df)
+
 
